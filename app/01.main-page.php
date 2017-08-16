91338119d95dac776e44325b5d2461c07a8648a2
Deffhyper <axelogykk@mail.ru>
2017-08-16 19:54:05 +0300

    base

---
+++ app/01.main-page.php
@@ -0,0 +1,151 @@
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include_once "parts/header.php" ?>
+
+
+<div class="slider" data-uk-slideshow="{animation:'random-fx', autoplay:true}">
+    <ul class="uk-slideshow uk-slideshow-fullscreen">
+        <li><img src="img/jpeg_files/andy-kerr-153378.jpg" alt="">
+
+            <div class="uk-overlay-panel">
+                <div class="uk-panel uk-panel-box ">
+                    <h2 class="">Hello User</h2>
+                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolorem earum enim iusto
+                        porro quia
+                        quibusdam voluptate? Accusantium est facilis, iusto molestiae nesciunt nihil odio officia
+                        officiis
+                        porro qui saepe?</p>
+                </div>
+            </div>
+        </li>
+        <li><img src="img/jpeg_files/davide-cantelli-139887.jpg" alt=""></li>
+        <li><img src="img/jpeg_files/james-donovan-180375.jpg" alt=""></li>
+    </ul>
+    <div class="my-container">
+        <a href="" class="uk-slidenav uk-slidenav-previous" data-uk-slideshow-item="previous"></a>
+        <a href="" class="uk-slidenav uk-slidenav-next" data-uk-slideshow-item="next"></a>
+    </div>
+</div>
+
+
+
+<div class="my-container padding-md">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <div class="uk-grid uk-grid-small" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-top', target:'.uk-panel', repeat:true, delay:300}">
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="my-container padding-md">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, exercitationem nemo? Ab accusamus aperiam aut
+        consequatur dolorum ex excepturi, expedita, fugit ipsam laborum maxime minima modi, pariatur quo repellendus.
+        Debitis.
+    </p>
+    <div class="uk-grid uk-grid-small" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-top', target:'.uk-panel', repeat:true, delay:300}">
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box uk-animation-1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box uk-animation-2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box uk-animation-3">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+        <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-grid-width-small-1-1">
+            <div class="uk-panel uk-panel-box uk-animation-4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda blanditiis dignissimos
+                    distinctio
+                    dolores enim et ex fugiat harum laboriosam maxime minus molestiae omnis optio quae, quam quisquam
+                    reprehenderit tempore.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "parts/footer.php" ?>
+
+<script src="js/libs.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
