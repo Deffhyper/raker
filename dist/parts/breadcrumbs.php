8c177cc952864785ae79d6f4018e4634601b3460
deff <olegkotsubko@gmail.com>
2017-09-13 19:32:49 +0300

    header, footer (20%)

---
+++ dist/parts/breadcrumbs.php
@@ -0,0 +1,17 @@
+<div class="breadcrumbs-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <ul class="breadcrumbs list-inline" itemscope itemtype="http://schema.org/BreadcrumbList">
+                    <li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+                        <a href="#" itemprop="item">
+                            <span itemprop="name">Главная</span>
+                        </a>
+                        <meta itemprop="position" content="1">
+                    </li>
+                    <li>Свежий улов</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
