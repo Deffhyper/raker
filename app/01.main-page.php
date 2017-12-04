b9a48f56c306b1102e288019882e5f42ed4027a2
deff <olegkotsubko@gmail.com>
2017-12-04 19:23:20 +0200

    add new design 20%

--- app/01.main-page.php
+++ app/01.main-page.php
@@ -192,7 +192,7 @@
         </div>
     </div>
 </div>
-<div class="main-shares">
+<!--<div class="main-shares">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 push-lg-4">
@@ -216,7 +216,7 @@
 
     <div class="main-shares__bg-1" style="background: url('img/jpeg_files/sh-bg-1.png') no-repeat center / cover"></div>
     <div class="main-shares__bg-2" style="background: url('img/jpeg_files/sh-2.png') no-repeat left / contain"></div>
-</div>
+</div>-->
 
 <div class="container">
     <div class="seo-text">
---
+++ app/07.main-new-page.php
@@ -0,0 +1,209 @@
+<?php include_once "parts/head.php" ?>
+<body>
+<?php include_once "parts/header.php" ?>
+
+<div class="container">
+    <ul class="block-list">
+        <li class="block-list__item">
+            <div class="goods-hrz">
+                <div class="goods-hrz__back">
+                    <img src="img/jpeg_files/a-1.jpg" alt="alter" width="" height="">
+                </div>
+                <div class="goods-hrz__right">
+                    <span class="goods-hrz__right--data">22 апреля 2017, 16:43</span>
+                    <a href="#" class="goods-hrz__right--title">Заголовок активности в 2 строки</a>
+
+                    <div class="goods-hrz__mobile">
+                        <img src="img/jpeg_files/a-1.jpg" alt="alter" width="" height="">
+                    </div>
+
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam corporis distinctio doloremque
+                        dolores error ex hic, ipsum minima nihil nostrum porro quae quo ratione repellendus repudiandae
+                        ut veniam? Beatae, ratione!
+                    </p>
+                    <p class="goods-hrz__right--link">
+                        <a href="#" class="btn btn__arrow btn__underline btn__regular "><span>Детальнее</span></a>
+                    </p>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+
+
+<div class="main-goods">
+    <div class="container">
+        <ul class="main-goods-list">
+            <li class="main-goods-list__item">
+                <div class="goods">
+                    <div class="goods__img">
+                        <a href="#">
+                            <object class="goods__img--label" data="img/svg/logo.svg" type="image/svg+xml" width="50"
+                                    height="50"></object>
+                            <img src="img/jpeg_files/goods-1.jpg" alt="goods" height="193" width="288">
+                        </a>
+                    </div>
+                    <div class="goods__title">
+                        <a href="#">Рецепты раков две строки</a>
+                    </div>
+                    <div class="goods__price">
+                        <p>245 грн <span>(300 г)</span></p>
+                    </div>
+                    <div class="goods__link">
+                        <a href="#" class="btn btn__border--hover btn__border btn__border--black">В корзину</a>
+                    </div>
+                    <div class="goods__info"><span>Только Киев</span></div>
+                    <div class="goods__time"><i class="iconmoon icon-clock"></i><span>10 дней 05:00:20</span></div>
+                </div>
+            </li>
+            <li class="main-goods-list__item">
+                <div class="goods">
+                    <div class="goods__img">
+                        <a href="#">
+                            <object class="goods__img--label" data="img/svg/logo.svg" type="image/svg+xml" width="50"
+                                    height="50"></object>
+                            <img src="img/jpeg_files/goods-1.jpg" alt="goods" height="193" width="288">
+                        </a>
+                    </div>
+                    <div class="goods__title">
+                        <a href="#">Рецепты раков две строки</a>
+                    </div>
+                    <div class="goods__price">
+                        <p>245 грн <span>(300 г)</span></p>
+                    </div>
+                    <div class="goods__link">
+                        <a href="#" class="btn btn__border--hover btn__border btn__border--black">В корзину</a>
+                    </div>
+                    <div class="goods__info"><span>Только Киев</span></div>
+                    <div class="goods__time"><i class="iconmoon icon-clock"></i><span>10 дней 05:00:20</span></div>
+                </div>
+            </li>
+            <li class="main-goods-list__item">
+                <div class="goods">
+                    <div class="goods__img">
+                        <a href="#">
+                            <object class="goods__img--label" data="img/svg/logo.svg" type="image/svg+xml" width="50"
+                                    height="50"></object>
+                            <img src="img/jpeg_files/goods-1.jpg" alt="goods" height="193" width="288">
+                        </a>
+                    </div>
+                    <div class="goods__title">
+                        <a href="#">Рецепты раков две строки</a>
+                    </div>
+                    <div class="goods__price">
+                        <p>245 грн <span>(300 г)</span></p>
+                    </div>
+                    <div class="goods__link">
+                        <a href="#" class="btn btn__border--hover btn__border btn__border--black">В корзину</a>
+                    </div>
+                    <div class="goods__info"><span>Только Киев</span></div>
+                    <div class="goods__time"><i class="iconmoon icon-clock"></i><span>10 дней 05:00:20</span></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="accent-block">
+    <div class="container">
+        <ul class="accent-block__list">
+            <li class="accent-block__list--item">
+                <p><b>Всегда свежие</b> <br>
+                    продукты</p>
+            </li>
+            <li class="accent-block__list--item">
+                <p><b>Приготовление</b> каждого <br>
+                    рецепта <b>с душой</b></p>
+            </li>
+            <li class="accent-block__list--item">
+                <p><b>Оперативная доставка</b><br>
+                    за 90 минут</p>
+            </li>
+            <li class="accent-block__list--item">
+                <p>С уважением <b><br>
+                        к каждому клиенту</b></p>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="main-shares">
+    <div class="main-shares__bg-1" style="background: url('img/jpeg_files/sh-bg-1.png') no-repeat center / cover"></div>
+    <div class="main-shares__bg-2" style="background: url('img/jpeg_files/bg_raker.png') no-repeat left / contain"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 push-lg-4">
+                <p class="main-shares__title">Угадывай счёт –
+                    выигрывай ящик раков!</p>
+
+                <p class="main-shares__subtitle">Футбольная акция от Raker.</p>
+                <a href="#" class="main-shares__link btn btn__border btn__border--black-hover">Участвовать!</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="section section--grayBack">
+    <div class="container container--grayBack">
+        <div class="seo-text">
+            <p>
+                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник
+                создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов.
+                Lorem
+                Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный
+                дизайн.
+                Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х
+                годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах
+                которых
+                используется Lorem Ipsum.
+            </p>
+            <br>
+
+            <h2>H2: Откуда он появился?</h2>
+
+            <p>
+                Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так.
+                Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий
+                назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из
+                самых странных слов в Lorem Ipsum, "consectetur", и занялся его поисками в классической латинской
+                литературе. В результате он нашёл неоспоримый первоисточник Lorem Ipsum в разделах 1.10.32 и 1.10.33
+                книги "de Finibus Bonorum et Malorum" ("О пределах добра и зла"), написанной Цицероном в 45 году
+                н.э. Этот трактат по теории этики был очень популярен в эпоху Возрождения. Первая строка Lorem
+                Ipsum, "Lorem ipsum dolor sit amet..", происходит от одной из строк в разделе 1.10.32
+            </p>
+
+            <p>
+                Классический текст Lorem Ipsum, используемый с XVI века, приведён ниже. Также даны разделы 1.10.32 и
+                1.10.33 "de Finibus Bonorum et Malorum" Цицерона и их английский перевод, сделанный H. Rackham, 1914
+                год.
+            </p>
+            <br>
+            <h2>Откуда он появился?</h2>
+
+            <p>
+                Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem
+                Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а
+                также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации
+                "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и
+                редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым
+                словам "lorem ipsum" сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего
+                рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по
+                ошибке, некоторые - намеренно (например, юмористические варианты).
+            </p>
+        </div>
+    </div>
+</section>
+
+
+
+<?php include_once "parts/footer.php" ?>
+
+<script src="js/libs.min.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
